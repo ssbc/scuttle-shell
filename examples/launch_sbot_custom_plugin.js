1fa83afef777f28f13f52b22b0c8ce690c8eecb0
andre alves garzia <andre@andregarzia.com>
2018-07-13 11:57:20 -0300

    better params

--- examples/launch_sbot_custom_plugin.js
+++ examples/launch_sbot_custom_plugin.js
@@ -2,6 +2,6 @@ let scuttleshell = require("../server")
 
 console.log("Starting sbot, quitting after 30 seconds")
 console.log("open http://localhost:8989/get-address")
-scuttleshell.start(false, ["./examples/service-discovery"])
+scuttleshell.start({ plugins: ["./examples/service-discovery"] })
 
 setTimeout(scuttleshell.stop, 30000)
